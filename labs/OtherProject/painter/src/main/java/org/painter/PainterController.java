adc950bc61887620961663755dc3c28fab873183
duongnguyen291 <2901nguyendinhduong@gmail.com>
2024-05-27 11:44:28 +0700

    Done painter

--- labs/OtherProject/painter/src/main/java/org/painter/PainterController.java
+++ labs/OtherProject/painter/src/main/java/org/painter/PainterController.java
@@ -4,16 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class PainterController {
-
+    private Color color = Color.BLACK;
     @FXML
     private Pane drawingAreaPane;
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4);
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, color);
         drawingAreaPane.getChildren().add(newCircle);
     }
 
@@ -22,4 +23,13 @@ public class PainterController {
         drawingAreaPane.getChildren().clear();
     }
 
+    @FXML
+    void changeColorToWhite() {
+        color = Color.WHITE;
+    }
+
+    @FXML
+    void changeColorToBlack() {
+        color = Color.BLACK;
+    }
 }
